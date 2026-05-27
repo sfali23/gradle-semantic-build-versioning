23d48e58aebe59c69a04aaae58e766aa0feedc80
sfali23 <f.syed.ali@gmail.com>
2026-05-27 09:14:34 -0400

    Moving cucumber tests to test directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import net.vivin.gradle.versioning.tasks.TagTask
+// import net.vivin.gradle.versioning.tasks.TagTask
 
 plugins {
     id("com.gradle.plugin-publish") version "1.3.0"
@@ -37,12 +37,12 @@ spotless {
     }
 }
 
-tasks.withType<TagTask> {
+/*tasks.withType<TagTask> {
     dependsOn(tasks.named("publishPlugins"))
 }
 tasks.named("publishPlugins").configure {
     dependsOn(tasks.named("build"))
-}
+}*/
 
 repositories {
     mavenCentral()
@@ -72,25 +72,18 @@ sourceSets.main
     .groovy
     .srcDir("src/main/groovy")
 
-// Create separate source sets for unit tests and integration tests
-sourceSets {
-    create("integrationTest") {
-        compileClasspath += sourceSets.main.get().output
-        runtimeClasspath += sourceSets.main.get().output
-    }
-}
-
 sourceSets.test
     .get()
     .java
     .srcDir("src/test/java")
 sourceSets.test
     .get()
+    .java
+    .srcDir("src/test/kotlin")
+sourceSets.test
+    .get()
     .resources
     .srcDir("src/test/resources")
-
-sourceSets["integrationTest"].java.srcDir("src/integrationTest/kotlin")
-sourceSets["integrationTest"].resources.srcDir("src/integrationTest/resources")
 
 val createPluginClasspathFile by tasks.registering {
     inputs.files(sourceSets.main.get().runtimeClasspath)
@@ -130,26 +123,15 @@ dependencies {
     testImplementation("com.typesafe:config:1.4.8")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
 
-    // Integration test dependencies (including Cucumber)
-    "integrationTestImplementation"("org.eclipse.jgit:org.eclipse.jgit.junit:7.6.0.202603022253-r")
-    "integrationTestImplementation"("org.junit.jupiter:junit-jupiter:5.10.0")
-    "integrationTestImplementation"("org.junit.platform:junit-platform-suite:1.10.0")
-    "integrationTestImplementation"("com.typesafe:config:1.4.8")
-    "integrationTestImplementation"("io.cucumber:cucumber-java:7.14.0")
-    "integrationTestImplementation"("io.cucumber:cucumber-junit-platform-engine:7.14.0")
-    "integrationTestImplementation"("io.cucumber:cucumber-expressions:16.1.2")
-    "integrationTestRuntimeOnly"("org.junit.platform:junit-platform-launcher:1.10.0")
-
-    // Integration tests need access to test source
-    "integrationTestImplementation"(sourceSets.test.get().output)
-    "integrationTestImplementation"(sourceSets.test.get().compileClasspath)
+    testImplementation("org.junit.platform:junit-platform-suite:1.10.0")
+    testImplementation("io.cucumber:cucumber-java:7.14.0")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:7.14.0")
+    testImplementation("io.cucumber:cucumber-expressions:16.1.2")
 
     add("jacocoRuntime", "org.jacoco:org.jacoco.agent:${jacoco.toolVersion}:runtime")
 
     testRuntimeOnly(files(createPluginClasspathFile.get()))
     testRuntimeOnly(files(createJacocoAgentClasspathFile.get()))
-    "integrationTestRuntimeOnly"(files(createPluginClasspathFile.get()))
-    "integrationTestRuntimeOnly"(files(createJacocoAgentClasspathFile.get()))
 }
 
 tasks.jacocoTestReport {
@@ -191,55 +173,27 @@ tasks.test {
     // Don't fail if no tests are discovered (test compatibility needs fixing)
     failOnNoDiscoveredTests = false
 
-    // Unit tests only - exclude cucumber engine
     useJUnitPlatform {
         includeEngines("junit-jupiter")
-        excludeEngines("cucumber")
+        includeEngines("cucumber")
     }
+
+    systemProperty("cucumber.junit-platform-engine.enabled", "true")
+    systemProperty("cucumber.features", "classpath:features")
+    systemProperty("cucumber.glue", "steps")
 }
-
-tasks.named<ProcessResources>("processIntegrationTestResources") {
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-// Create integration test task for cucumber tests
-val integrationTest =
-    tasks.register<Test>("integrationTest") {
-        description = "Runs integration tests (including Cucumber)"
-        group = "verification"
-
-        testClassesDirs = sourceSets["integrationTest"].output.classesDirs
-        classpath = sourceSets["integrationTest"].runtimeClasspath
-
-        if (System.getenv("CIRCLECI") != null) {
-            maxHeapSize = "1G"
-        }
-
-        // Don't fail if no tests are discovered
-        failOnNoDiscoveredTests = false
-
-        useJUnitPlatform {
-            includeEngines("junit-jupiter", "cucumber")
-        }
-
-        systemProperty("cucumber.junit-platform-engine.enabled", "true")
-        systemProperty("cucumber.features", "classpath:features")
-        systemProperty("cucumber.glue", "steps")
-
-        shouldRunAfter(tasks.test)
-    }
 
 tasks.withType<Jar>().configureEach {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 gradlePlugin {
-    website = "https://github.com/vivin/gradle-semantic-build-versioning"
-    vcsUrl = "https://github.com/vivin/gradle-semantic-build-versioning"
+    website = "https://github.com/sfali23/gradle-semantic-build-versioning"
+    vcsUrl = "https://github.com/sfali23/gradle-semantic-build-versioning"
     plugins {
         create("semanticBuildVersioningPlugin") {
-            id = "net.vivin.gradle-semantic-build-versioning"
-            implementationClass = "net.vivin.gradle.versioning.SemanticBuildVersioningPlugin"
+            id = "io.github.sfali23.gradle-semantic-build-versioning"
+            implementationClass = "semverrelease.SemanticBuildVersioningPlugin"
             displayName = "Gradle Semantic Build Versioning Plugin"
             description =
                 "This is a Gradle settings-plugin that provides support for semantic versioning of builds. It is quite easy to use and extremely configurable. The plugin allows you to bump the major, minor, patch or pre-release version based on the latest version, which is identified from a git tag. It also allows you to bump pre-release versions based on a scheme that you define. The version can be bumped by using version-component-specific project properties or can be bumped automatically based on the contents of a commit message. If no manual bumping is done via commit message or project property, the plugin will increment the version-component with the lowest precedence; this is usually the patch version, but can be the pre-release version if the latest version is a pre-release one. The plugin does its best to ensure that you do not accidentally violate semver rules while generating your versions; in cases where this might happen the plugin forces you to be explicit about violating these rules. As this is a settings plugin, it is applied to settings.gradle and version calculation is therefore performed right at the start of the build, before any projects are configured. This means that the project version is immediately available (almost as if it were set explicitly - which it effectively is), and will never change during the build (barring some other, external task that attempts to modify the version during the build). While the build is running, tagging or changing the project properties will not influence the version that was calculated at the start of the build."
