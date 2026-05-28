15d389d38dd70f9f6e02d11dc313c1cfc8ae9251
sfali23 <f.syed.ali@gmail.com>
2026-05-28 10:48:10 -0400

    Remove `signingKeyId` from signing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -253,11 +253,10 @@ publishing {
 }
 
 signing {
-    val signingKeyId: String? by project
     val signingKey: String? by project
     val signingPassword: String? by project
 
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
 
